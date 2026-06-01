52eb609f1e892d06076bf573ed0947e1dde59278
Ramadeep_Yadav <rdeep1208y@gmail.com>
2026-06-01 08:23:08 +0530

    Add files via upload

---
+++ 33_Siblings_and_Parent/script.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstChild)
+let a = document.body.firstChild
+console.log(a.firstChild)
+console.log(document.body.firstChild.firstChild.parentElement)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
