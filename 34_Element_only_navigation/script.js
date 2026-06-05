d7d1dbddf7b72e2ff7f5917edf5773854c0dd378
Ramadeep_Yadav <rdeep1208y@gmail.com>
2026-06-05 23:09:09 +0530

    Add files via upload

---
+++ 34_Element_only_navigation/script.js
@@ -0,0 +1,8 @@
+const changecolor = ()=>{
+    document.body.firstElementChild.firstElementChild.style.background = "red"
+}
+
+
+let b = document.body
+console.log("First Child of b : " , b.firstChild)
+console.log("First Element Child of b : " , b.firstElementChild)
