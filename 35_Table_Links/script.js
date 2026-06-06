7f0239f932cd374675833486467fe646e22b2fa2
Ramadeep_Yadav <rdeep1208y@gmail.com>
2026-06-06 22:33:07 +0530

    Add files via upload

---
+++ 35_Table_Links/script.js
@@ -0,0 +1,14 @@
+let a = document.body.firstElementChild.firstElementChild
+console.log(a)
+console.log(a.rows)
+console.log(typeof a.rows)
+
+console.log(a.caption)
+console.log(a.tHead.firstElementChild)
+console.log(a.tFoot)
+console.log(a.tBodies)
+
+console.log(a.rows[1].rowIndex)
+
+console.log(typeof windows)
+console.log(typeof documents)
