1188d9fa3f8aad0e761614779702cfd74517b8ef
Ramadeep_Yadav <rdeep1208y@gmail.com>
2026-06-18 21:57:42 +0530

    Delete 37_PrasticeSet_07 directory

--- 37_PrasticeSet_07/script.js
+++
@@ -1,9 +0,0 @@
-let cg0 = document.querySelectorAll(".navbar")[0].firstElementChild
-cg0.style.color = "red"
-
-document.querySelectorAll(".navbar")[0].children[0].style.color = "green"
-document.querySelectorAll(".navbar")[0].children[2].style.color = "green"
-
-Array.from(document.getElementsByTagName("li")).forEach((Elem)=>{
-    Elem.style.background = "cyan";
-})
